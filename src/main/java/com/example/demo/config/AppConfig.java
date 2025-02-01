fb67d80ce3b4eaf602303fbfe86d876f223c8dac
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-02-01 15:37:10 +0530

    Create AppConfig.java

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/demo/config/AppConfig.java
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
