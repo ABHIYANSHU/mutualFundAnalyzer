63f36bb7877d1ba7994e005ef3924163466ac1e9
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-02-01 15:48:12 +0530

    Create MfController.java

---
+++ src/main/java/com/example/demo/controller/MfController.java
@@ -0,0 +1,23 @@
+// src/main/java/com/example/demo/controller/MfController.java
+import com.example.demo.model.MfDetails;
+import com.example.demo.service.MfService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
+public class MfController {
+    private final MfService mfService;
+
+    public MfController(MfService mfService) {
+        this.mfService = mfService;
+    }
+
+    @GetMapping("/mf")
+    public String getMfDetails(Model model) {
+        List<MfDetails> allDetails = mfService.getAllSchemeDetails();
+        model.addAttribute("allDetails", allDetails);
+        return "mf-details";
+    }
+}
