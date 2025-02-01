3b4aa559517d0f018119fc7d5237337a41e305f8
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-02-01 15:36:11 +0530

    Create MfDetails.java

---
+++ src/main/java/com/example/demo/model/MfDetails.java
@@ -0,0 +1,24 @@
+// src/main/java/com/example/demo/model/MfDetails.java
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class MfDetails {
+    private Meta meta;
+    private List<DataPoint> data;
+    
+    @Data
+    public static class Meta {
+        private String fund_house;
+        private String scheme_type;
+        private String scheme_category;
+        private String scheme_code;
+        private String scheme_name;
+    }
+    
+    @Data
+    public static class DataPoint {
+        private String date;
+        private String nav;
+    }
+}
