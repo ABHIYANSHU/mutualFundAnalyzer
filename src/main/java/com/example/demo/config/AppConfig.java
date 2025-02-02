da42043b1f508a173b7f2c33a5c2b93cb7bd3c9e
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-02-02 14:03:36 +0530

    Update AppConfig.java

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
