b1d775ab14959bf395266c4a8c25bf18db9011b9
Abhinav Srivastava <abhiyanshu11@gmail.com>
2025-02-02 13:47:00 +0530

    Update AppConfig.java

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -5,8 +5,17 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+    
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+            .setConnectTimeout(Duration.ofSeconds(5))
+            .setReadTimeout(Duration.ofSeconds(60))
+            .build();
     }
 }
